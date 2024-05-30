03c7593b7f4802ccaabca144f922cd604db825f0
GastonnRomero <gaston.romero.dev@gmail.com>
2024-05-30 14:52:42 +0000

    restructurando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  items-center p-auto">
-      <div className="flex-1 absolute">
-        <img className="w-screen h-screen object-cover" src="/construccionzwol.jpeg"/>
-      </div>
+    <main>
+      <div className="h-screen flex flex-col items-center justify-center text-center">
+        <div className="flex-1 absolute">
+          <img className="w-screen h-screen object-cover brightness-50" src="/soldzwol.jpeg" alt="robotic welding" />
+        </div>
 
-      <div className="w-full relative max-w-5xl items-center justify-between text-sm lg:flex bg-transparent -bottom-10">
-        <p className="font-extrabold text-center text-xl text-zinc-[#343338] ">En Construcción!</p>
+        <div className="flex flex-col items-center relative  justify-between ">
+          <img className="max-w-32" src="/zwol.png" alt="logo" />
+          <h1 className="text-4xl text-white font-bold">Próximamente</h1>
+        </div>
       </div>
     </main>
   );
