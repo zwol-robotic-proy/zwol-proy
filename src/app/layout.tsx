dda8ceb8193083190357eb7d8ee378d97bd12db9
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-05-30 16:51:29 -0300

    reparado icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es-419">
-      <link rel="apple-touch-icon" href="/icon.png" type="image/png"/>
+      <link rel="apple-touch-icon" href="/icon" type="image/png"/>
+      <link rel="icon" href="/favicon.ico" />
       <body className={inter.className}>{children}</body>
     </html>
   );
