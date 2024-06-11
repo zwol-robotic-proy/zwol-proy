3af22b549d550f799d6f2f9d9e9266b5394ee4b1
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-06-11 17:13:13 -0300

    add cards examples

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -1,8 +1,71 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
 const Productos = () => {
     return (
-        <div className="h-screen w-full flex items-center justify-center">
-            <p className="font-semibold text-6xl">Esto es... Productos</p>
-        </div>
+        <main className="mt-20 mx-auto max-w-7xl h-screen">
+            <div className="grid grid-cols-3 gap-7">
+                <Card className="border border-transparent hover:border-zwol-1">
+                    <CardHeader>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p>Card Content</p>
+                    </CardContent>
+                    <CardFooter>
+                        <p>Card Footer</p>
+                    </CardFooter>
+                </Card>
+                <Card className="border border-transparent hover:border-zwol-1">
+                    <CardHeader>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p>Card Content</p>
+                    </CardContent>
+                    <CardFooter>
+                        <p>Card Footer</p>
+                    </CardFooter>
+                </Card>
+                <Card className="border border-transparent hover:border-zwol-1">
+                    <CardHeader>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p>Card Content</p>
+                    </CardContent>
+                    <CardFooter>
+                        <p>Card Footer</p>
+                    </CardFooter>
+                </Card>
+                <Card className="border border-transparent hover:border-zwol-1">
+                    <CardHeader>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p>Card Content</p>
+                    </CardContent>
+                    <CardFooter>
+                        <p>Card Footer</p>
+                    </CardFooter>
+                </Card>
+                <Card className="border border-transparent hover:border-zwol-1">
+                    <CardHeader>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p>Card Content</p>
+                    </CardContent>
+                    <CardFooter>
+                        <p>Card Footer</p>
+                    </CardFooter>
+                </Card>
+            </div>
+        </main>
     )
 };
 
