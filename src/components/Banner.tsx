f6815232ceaacce5eedac46584884c1b02d2d703
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-06-12 01:21:06 -0300

    try push branch

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -23,7 +23,7 @@ const Banner = () => {
                     </div>
                     {/* <!-- Image Div --> */}
                     <div>
-                        <img src="https://plus.unsplash.com/premium_photo-1682144827510-bbe3cd636253?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D  className="mx-auto inline-block h-full w-full />
+                        <img src="https://plus.unsplash.com/premium_photo-1682144827510-bbe3cd636253?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  className="mx-auto inline-block h-full w-full" />
                     </div>
                 </div>
             </div>
