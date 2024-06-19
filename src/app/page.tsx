3e6a96832625e48764c8047357ef167c250b8d48
GastonnRomero <gaston.romero.dev@gmail.com>
2024-06-19 01:19:06 +0000

    trabajando en el banner y front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import Carousel from "@/components/Carousel";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <main>
+        <Carousel />
         <Banner />
       </main>
       <Footer />
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,16 +2,17 @@ import ButtonProy from "./ButtonProy";
 
 const Banner = () => {
     return(
-        <section className="relative bg-gradient-to-r from-violet-50 to-orange-50">
+        <section className="relative">
             <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
                 <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2 [&_div]:justify-center">
                     <div className="max-w-[720px] nth-3:text-center">
                         <h1 className="mb-3 pb-4 text-4xl font-bold text-zwol-1 md:text-6xl">Desarrollando innovación</h1>
-                        <p className="mb-6 max-w-[528px] text-xl md:mb-10 text-justify">Sumérgete en el mundo de la tecnología con nuestro fabricante argentino de robots líder en Latinoamérica. Descubre la excelencia, la precisión y la innovación que solo nosotros ofrecemos. Con soluciones a la medida de tus necesidades, estamos aquí para potenciar el futuro de tu empresa. ¡Conviértete en pionero de la automatización con nosotros!</p>
+                        <p className="mb-6 max-w-[528px] text-xl md:mb-10 text-justify">Sumérgete en el mundo de la Tecnología con nuestros Robot de Fabricación íntegramente Nacional.
+                        Descubre la excelencia, la precisión, innovación y adaptabilidad que estamos dispuestos a compartir, soluciones a la medida de las necesidades, estamos aquí para potenciar le tecnificación de tu empresa, no dudes en consultarnos!</p>
                         <ButtonProy hRef="/" styleP="flex text-center">Showroom</ButtonProy>
                     </div>
                     <div>
-                        <img src="https://plus.unsplash.com/premium_photo-1682144827510-bbe3cd636253?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  className="mx-auto inline-block h-full w-full" />
+                        <img src="/zwolsold.jpg"  className="mx-auto inline-block h-full w-full" />
                     </div>
                 </div>
             </div>
