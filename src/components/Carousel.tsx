b2ba0b4616c39cdc1a34f2673007f10b070d2f32
GastonnRomero <gaston.romero.dev@gmail.com>
2024-07-14 18:44:18 +0000

    trabajando en el disenio y realizando cambios de ruta en el footer, cambio de imagen en el banner y agregando la seccion de iconos de redes en el footer

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -17,7 +17,7 @@ const CarouselComponent = () => {
     return (
         <Carousel
             plugins={[plugin.current]}
-            className="pt-20"
+            className="pt-[4.4rem]"
             onMouseEnter={plugin.current.stop}
             onMouseLeave={plugin.current.reset}
         >
