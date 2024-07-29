db936794663249f44909c0dde4a36db86d577fa3
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-07-28 23:19:47 -0300

    add now changes in images

--- src/app/productos/@modal/(.)p/[id]/modal.tsx
+++ src/app/productos/@modal/(.)p/[id]/modal.tsx
@@ -25,9 +25,9 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
     <div className={`select-none`} >
       <div className="absolute w-full top-0 -bottom-[57%] bg-black/50 z-[1001]" onClick={onDismiss} />
-      <dialog ref={dialogRef} className="min-w-80 min-h-80 lg:min-w-[500px] lg:min-h-[500px] border-none rounded-xl flex justify-center items-center size-12 font-medium nth-1:object-contain">
+      <dialog ref={dialogRef} className="min-w-80 min-h-80 lg:min-w-[500px] lg:min-h-[500px] border-none rounded-xl flex flex-col justify-center items-center size-12 font-medium nth-1:object-contain">
         {children}
-        <div className='w-full h-10 bottom-0 flex items-center justify-center absolute hover:bg-zwol-1/50 hover:text-white'>
+        <div className='w-full h-10flex items-center justify-center hover:bg-zwol-1/50 hover:text-white'>
           <button className='text-sm h-full w-full' onClick={handleReload}>Más Información...</button>
         </div>
         <button onClick={onDismiss} className="absolute top-2.5 right-2.5 p-1 rounded-full bg-transparent hover:bg-zwol-1 after:hover:text-white cursor-pointer flex items-center justify-center font-medium size-6 after:text-zwol-1 after:content-['X']" />
--- src/app/productos/@modal/(.)p/[id]/page.tsx
+++ src/app/productos/@modal/(.)p/[id]/page.tsx
@@ -1,15 +1,23 @@
 import { Modal } from './modal';
 import { CARDS } from '@/lib/utils';
-import CARSINFO from '@/DDBB/CARDS_INFO.json';
+import CARDSINFO from '@/DDBB/CARDS_INFO.json';
 import Image from 'next/image';
 
 export default function ProductModal({ params: { id: productId } }: { params: { id: string } }) {
 
-  const cards = CARSINFO[0].productos.find((card: CARDS) => card.id === productId);
+  const cards = CARDSINFO[0].productos.find((card: CARDS) => card.id === productId);
   if (!cards) return null;
-    
+
   return (
     <Modal>
-        <Image width={1900} height={1900} src={cards.IMG} alt={cards.title} />
+      <div className="snap-x flex snap-mandatory h-full w-full mx-auto overflow-scroll">
+        <div className="snap-start w-full h-full flex-shrink-0 flex items-center justify-center">
+          <Image width={1900} height={1900} alt={`${productId}01`} src={`/prod/${productId}/0001-1.jpg`} />
+        </div>
+        <div className="snap-start w-full h-full flex-shrink-0 flex items-center justify-center">
+        <Image width={1900} height={1900} alt={`${productId}02`} src={`/prod/${productId}/0001.jpg`} />
+        </div>
+      </div>
     </Modal>
-)};
+  )
+};
